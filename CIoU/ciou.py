56cbd243742fc2bfdc9259cc08985cc0ea7ced47
tuongtranngoc <ngoctuongwindy@gmail.com>
2023-10-21 10:48:09 +0700

    update: ciou

---
+++ CIoU/ciou.py
@@ -0,0 +1,46 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import torch
+
+
+def compute_loss(target_bboxes, pred_bboxes):
+    """Reference: https://arxiv.org/pdf/1911.08287.pdf
+    Args:
+        target_bboxes (torch.Tensor): Target boundong boxes, [N, H, W, 4]
+        pred_bboxes (torch.Tensor): Predicted bounding boxes, [N, H,W, 4]
+    """
+    # Compute intersections
+    x1 = torch.max(target_bboxes[..., 0], pred_bboxes[..., 0])
+    y1 = torch.max(target_bboxes[..., 1], pred_bboxes[..., 1])
+    x2 = torch.min(target_bboxes[..., 2], pred_bboxes[..., 2])
+    y2 = torch.min(target_bboxes[..., 3], pred_bboxes[..., 3])
+
+    instersects = torch.clamp(x2-x1, min=0) * torch.clamp(y2-y1, min=0)
+
+    # Compute unions
+    A = abs((target_bboxes[..., 2]-target_bboxes[..., 0]) * target_bboxes[..., 3]-target_bboxes[..., 1])
+    B = abs((pred_bboxes[..., 2]-pred_bboxes[..., 0]) * pred_bboxes[..., 3]-pred_bboxes[..., 1])
+
+    unions = A + B - instersects
+
+    ious = instersects / unions
+
+    cx1 = torch.min(target_bboxes[..., 0], pred_bboxes[..., 0])
+    cy1 = torch.min(target_bboxes[..., 1], pred_bboxes[..., 1])
+    cx2 = torch.max(target_bboxes[..., 2], pred_bboxes[..., 2])
+    cy2 = torch.max(target_bboxes[..., 3], pred_bboxes[..., 3])
+
+    # Compute Euclidean between central points and diagonal lenght
+    c_dist = ((target_bboxes[..., 2] + target_bboxes[..., 0] - pred_bboxes[..., 2] - pred_bboxes[..., 0]) ** 2 + \
+              (target_bboxes[..., 3] + target_bboxes[..., 1] - pred_bboxes[..., 3] - pred_bboxes[..., 1]) ** 2) / 4
+    diagonal_l2 = torch.clamp((cx2-cx1), 0) **2 + torch.clamp((cy2-cy1), 0) ** 2
+
+    # Postive trade-off parameter and asspect ratio
+    v = 4 * (torch.arctan((target_bboxes[..., 2]-target_bboxes[..., 0])/(target_bboxes[..., 3]-target_bboxes[..., 1]))- \
+             torch.arctan((pred_bboxes[..., 2]-pred_bboxes[..., 0])/(pred_bboxes[..., 3]-pred_bboxes[..., 1]))) ** 2 / (torch.pi**2)
+    alpha = v / (1-ious+v)
+
+    return ious - c_dist / diagonal_l2 + v * alpha
+
